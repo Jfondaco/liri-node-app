5ddcb81257f9f25d41661ccc7010b7120432ef97
Joey Fondaco <Jfondacoo@gmail.com>
2019-01-29 00:15:53 -0500

    finished version

--- liri.js
+++ liri.js
@@ -11,12 +11,18 @@ var spotify = new Spotify(keys.spotify);
 //  spotify-this-song
 //  movie-this
 //  do-what-it-says
-0
-var com02
-command = process.argv[2];
+
+
+var command = process.argv[2];
 var input = process.argv.slice(3).join(" ");
+console.log("command: "+command);
+console.log("input: "+input);
 
 if (command === "concert-this"){
+    concertSearch();
+}
+
+function concertSearch(){
     axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
     .then(function (response) {
       console.log(response);
@@ -27,6 +33,10 @@ if (command === "spotify-this-song"){
     if (input === ""){
         input = "The Sign Ace of Base";
     }
+    spotSearch();
+}
+
+function spotSearch(){
     spotify
   .search({ type: 'track', query: input })
   .then(function(response) {
@@ -39,7 +49,10 @@ if (command === "spotify-this-song"){
     console.log("Song: " + song);
     // song preview
     var preview = response.tracks.items[0].preview_url
+    if (preview != null){
     console.log("Preview URL: " + preview);
+    }
+    
     //album name
     var album = response.tracks.items[0].album.name;
     console.log("Album: " + album);
@@ -55,27 +68,30 @@ if (command === "movie-this"){
     if (input === ""){
         input = "Mr Nobody";
     }
+    omdbSearch();
+    
+}
+function omdbSearch(){
     axios.get("http://www.omdbapi.com/?t="+ input +"&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    var movieTitle = response.data.Title;
-    console.log("Title: "+movieTitle);
-    var movieYear = response.data.Year;
-    console.log("Release Year: " + movieYear)
-    var imdb = response.data.imdbRating;
-    console.log("The movie's IMDB rating is: " + imdb);
-    var tomato = response.data.Ratings[1].Value;
-    console.log("RottenTomato's Rating: " + tomato);
-    var movieCountry = response.data.Country;
-    console.log("Country: " + movieCountry);
-    var movieLanguage = response.data.Language;
-    console.log("Language: "+ movieLanguage);
-    var moviePlot = response.data.Plot;
-    console.log("Plot: " + moviePlot);
-    var movieActors = response.data.Actors;
-    console.log("Actors: "+ movieActors);
-  }
-);
-
+        function(response) {
+            var movieTitle = response.data.Title;
+            console.log("Title: "+movieTitle);
+            var movieYear = response.data.Year;
+            console.log("Release Year: " + movieYear)
+            var imdb = response.data.imdbRating;
+            console.log("The movie's IMDB rating is: " + imdb);
+            var tomato = response.data.Ratings[1].Value;
+            console.log("RottenTomato's Rating: " + tomato);
+            var movieCountry = response.data.Country;
+            console.log("Country: " + movieCountry);
+            var movieLanguage = response.data.Language;
+            console.log("Language: "+ movieLanguage);
+            var moviePlot = response.data.Plot;
+            console.log("Plot: " + moviePlot);
+            var movieActors = response.data.Actors;
+            console.log("Actors: "+ movieActors);
+        }
+    );
 }
 
 if (command === "do-what-it-says"){
@@ -83,9 +99,27 @@ if (command === "do-what-it-says"){
         if (error) {
           return console.log(error);
         }        
-        console.log(data);
         var dataArr = data.split(",");
-        console.log(dataArr);
+        command = dataArr[0];
+        input = dataArr[1];
+            if (command === "spotify-this-song"){
+                if (input === ""){
+                input = "The Sign Ace of Base";
+                }
+                spotSearch();
+            }
+
+            if (command === "movie-this"){
+                if (input === ""){
+                input = "Mr Nobody";
+                }
+                omdbSearch();
+                
+            }
+            if (command === "concert-this"){
+                concertSearch();
+            }
+        
       
       });
       
